ea78d97076e62cf581be67bd1d5938d7052e2bb8
JunhoBaek <bemodesty306@naver.com>
2022-04-30 13:39:31 +0900

    server error handling

--- src/app.ts
+++ src/app.ts
@@ -8,6 +8,16 @@ const app = express();
 
 app.use("/post", postRouter);
 
+app.use((req, res, next) => {
+  const error = new Error(`404 ${req.method} ${req.url} no router`);
+  next(error);
+});
+
+app.use((err: any, req: any, res: any, next: any) => {
+  res.status(500);
+  res.send("server error");
+});
+
 sequelize.sync().then(() => {
   app.listen(8080, () => {
     console.log("8080");
